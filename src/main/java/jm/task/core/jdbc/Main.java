12657721e03e0ea7ee20a43041438c4f88a6353a
Feshest <feshestt@gmail.com>
2022-12-04 15:34:47 +0300

    Solution

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Ryan", "Gosling", (byte) 42);
+        userService.saveUser("Brad", "Pitt", (byte) 58);
+        userService.saveUser("Leonardo", "Dicaprio", (byte) 48);
+        userService.saveUser("Matthew", "Mcconaughey", (byte) 53);
+
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
